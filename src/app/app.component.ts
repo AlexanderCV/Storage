253bc256418a1e74019027b48ad2b35072728fd8
Emilio_Vega <acapcha@outlook.com>
2022-07-25 02:00:05 -0500

    Subiendo proyecto de firebase Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage, ref , uploadBytes , listAll ,getDownloadURL } from '@angular/fire/storage'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'storage';
+export class AppComponent implements OnInit {
+
+  images: string[];
+
+  constructor(private storage:Storage){
+    
+    this.images = [];
+  }
+  ngOnInit(): void {
+    this.getImages();
+  }
+  uploadImage($event: any){
+
+  const file = $event.target.files[0];
+  console.log(file);
+  
+  // ACA SE TIENE LA REFERENCIA EL ESPACIO DONDE SE TIENE LA IMAGEN
+  const imgRef =  ref(this.storage,`images/${file.name}`); 
+
+  //SUBIENDO LAS IMÁGENES
+  uploadBytes(imgRef, file)
+    .then( response => {
+      console.log(response)
+      this.getImages();
+    })
+    .catch(err => console.log(err));
+
+  }
+
+  getImages(){
+    const imagesRef = ref(this.storage, 'images');
+
+    listAll(imagesRef)
+      .then (async response =>{
+        console.log(response);
+        this.images = [];
+
+        for (const item of response.items) {
+          const url = await getDownloadURL(item);
+          this.images.push(url);
+        }
+
+    }).catch(error => console.log(error));
+  }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'gdi-storage-2c320',
+    appId: '1:618155618246:web:53367e440a511954008cc7',
+    storageBucket: 'gdi-storage-2c320.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDo9fZlCWUxhlcYPp764bQKJY4P95VUkJ0',
+    authDomain: 'gdi-storage-2c320.firebaseapp.com',
+    messagingSenderId: '618155618246',
+  },
   production: true
 };
